cd563128efa3cc3616334f9a9483cb001e6d278c
dredington21 <126722599+dredington21@users.noreply.github.com>
2023-12-13 16:58:20 -0500

    Add files via upload

---
+++ MyGame/src/main/GamePanel.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+
+	// Screen Settings
+	final int originalTileSize = 16;
+	final int scale = 3;
+	
+	final int tileSize = originalTileSize * scale;
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = tileSize * maxScreenCol;
+	final int screenHeight = tileSize * maxScreenRow;
+	
+}
