21e783d71bd3fd73eb17976cf2bcb98d0601a17a
dredington21 <126722599+dredington21@users.noreply.github.com>
2023-12-16 13:01:26 -0500

    Add files via upload

--- MyGame/src/main/Main.java
+++ MyGame/src/main/Main.java
@@ -7,7 +7,14 @@ public class Main {
 		window.setResizable(false);
 		window.setTitle("2D adventure");
 		
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		
+		window.pack();
+		
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
+		
+		gamePanel.startGameThread();
 }
 }
